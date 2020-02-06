658b02a8c32a8e4bc608cafed7c31fc68e33d631
João Henrique <jenryk9503@gmail.com>
2020-02-06 14:57:40 -0300

    Design fixes

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 const WeatherCard = (props) => {
     return (
         <div className='card' > 
-            <h1 className="city" >São Paulo</h1>   
-            <h3 className="country">BR</h3>    
+            <div className="location">
+                <h1 className='city' >São Paulo</h1>   
+                <h3 className="country">BR</h3>    
+            </div>
             <img className="icon" src="" alt="Weather Icon"></img> 
             <h1 className="temp"> 25 °C</h1>
             <h1 className="condition">Clouds</h1>
